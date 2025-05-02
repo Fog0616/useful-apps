9031737c9a0795f9bc3fb2a3ec15174d92cc8bd6
Cuiti <cuiti708@gmail.com>
2025-05-02 21:11:12 +0800

    三角函数！我们有救了！

---
+++ asa121_/Readme.md
@@ -0,0 +1,3 @@
+# 太棒了是三角函数，我们有救了！！！
+https://people.sc.fsu.edu/~jburkardt/cpp_src/asa121/asa121.html
+↑这网站很好啊
---
+++ asa121_/asa121.hpp
@@ -0,0 +1,2 @@
+double trigamma ( double x, int *ifault );
+void trigamma_values ( int *n_data, double *x, double *fx );
